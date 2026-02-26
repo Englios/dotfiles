b779aeee70508e286569cfb45e20a246ecfb1556
Englios <englios656@gmail.com>
2026-02-26 10:36:06 +0800

    Fixed waybar bluetooth issues and add fuzzel customizations

--- waybar/waybar/scripts/bluetooth-status.sh
+++ waybar/waybar/scripts/bluetooth-status.sh
@@ -5,7 +5,14 @@ if ! bluetoothctl show | grep -q "Powered: yes"; then
     exit 0
 fi
 
-mapfile -t connected < <(bluetoothctl devices Connected)
+# Get connected devices directly via D-Bus - no StartDiscovery triggered
+connected_json=$(dbus-send --system --print-reply \
+    --dest=org.bluez / \
+    org.freedesktop.DBus.ObjectManager.GetManagedObjects 2>/dev/null)
+
+mapfile -t connected < <(echo "$connected_json" | \
+    awk '/object path.*dev_/{mac=$NF} /"Connected"/{f=1} f && /boolean true/{print mac; f=0}' | \
+    tr -d '"' | sed 's|/org/bluez/hci[0-9]*/dev_||')
 
 if [ ${#connected[@]} -eq 0 ]; then
     jq -nc '{text: "󰂯", tooltip: "No devices connected", class: "on"}'
@@ -13,10 +20,13 @@ if [ ${#connected[@]} -eq 0 ]; then
 fi
 
 tooltip=" Connected"$'\n'
-for entry in "${connected[@]}"; do
-    mac=$(echo "$entry" | awk '{print $2}')
-    name=$(echo "$entry" | cut -d' ' -f3-)
-    battery=$(bluetoothctl info "$mac" 2>/dev/null | awk '/Battery Percentage/ {gsub(/[()]/,"",$NF); print $NF"%"}')
+for dev_path in "${connected[@]}"; do
+    mac=$(echo "$dev_path" | tr '_' ':')
+    name=$(echo "$connected_json" | awk -v d="$dev_path" \
+        '$0~d{f=1} f && /"Name"/{gsub(/.*string "/,""); gsub(/"$/,""); print; exit}')
+    [ -z "$name" ] && name="$mac"
+    battery=$(bluetoothctl info "$mac" 2>/dev/null | \
+        awk '/Battery Percentage/ {gsub(/[()]/,"",$NF); print $NF"%"}')
     if [ -n "$battery" ]; then
         tooltip+="  ${name}: ${battery}"$'\n'
     else
